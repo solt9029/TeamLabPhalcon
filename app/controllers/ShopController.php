6294024c707060f0338604ae4f0ef2a57e4120fd
Kenshi Shiode <atemu1212@yahoo.co.jp>
2017-08-06 19:34:57 +0900

    view files were added for shop

--- app/controllers/ShopController.php
+++ app/controllers/ShopController.php
@@ -6,4 +6,28 @@ class ShopController extends Controller{
 	public function indexAction(){
 
 	}
+
+	public function createAction(){
+
+	}
+
+	public function editAction(){
+
+	}
+
+	public function getAction(){
+
+	}
+
+	public function storeAction(){
+
+	}
+
+	public function updateAction(){
+
+	}
+
+	public function destroyAction(){
+		
+	}
 }
